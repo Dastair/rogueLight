c64c7af7a85c88e6c401fee3cd068f4f2c803416
Dastair <nesmcq@gmail.com>
2023-12-08 16:40:58 -0700

    lighting, flashlight, and cover mode

---
+++ playerCharacter/PointLight2D2.gd
@@ -0,0 +1,25 @@
+extends PointLight2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.texture_scale = 0.0
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		if self.texture_scale > 0.0:
+			self.texture_scale -= 0.1
+	else:
+		if self.texture_scale < 0.4:
+			self.texture_scale += 0.1
+		
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		look_at(get_global_mouse_position())
+		self.texture = preload("res://Resources/lightParticles/flash.png")
+		self.offset.x = 15
+		self.texture_scale = 0.8
+	else:
+		self.offset.x = 0
+		self.texture = preload("res://.godot/imported/pointLight.webp-631b7ae0c928feba6faed6027f2cf433.ctex")
--- playerCharacter/player.gd
+++ playerCharacter/player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
+@onready var point_light_2d = $PointLight2D as PointLight2D
+@onready var point_light_2d_2 = $PointLight2D2 as PointLight2D
 
 @export var speed: float = 100
+
+
 
 func get_input():
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_dir * speed
 
+
 func _physics_process(delta):
 	get_input()
-	move_and_collide(velocity * delta)
+	move_and_slide()
