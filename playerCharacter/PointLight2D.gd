db8568ba920296fd3c4c7735e900e74c22f65130
Dastair <nesmcq@gmail.com>
2023-12-09 15:22:07 -0700

    added hitboxes to light for future uses

--- playerCharacter/PointLight2D.gd
+++ playerCharacter/PointLight2D.gd
@@ -1,5 +1,9 @@
 extends PointLight2D
 
+@onready var enemy_light = $"../PointLightNoShadows/EnemyLight" as Area2D
+@onready var circleLight = $"../PointLightNoShadows/EnemyLight/circleShape" as CollisionShape2D
+@onready var flash_shape = $"../PointLightNoShadows/EnemyLight/flashShape" as CollisionPolygon2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,16 +15,21 @@ func _process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if self.texture_scale > 0.1:
 			self.texture_scale -= 0.1
-		print(self.texture_scale)
 	else:
 		if self.texture_scale < 0.4:
 			self.texture_scale += 0.1
-		
+	if Input.is_action_just_released("rightMouse"):
+		self.texture_scale = 0.4
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		look_at(get_global_mouse_position())
 		self.texture = preload("res://Resources/lightParticles/flash.png")
+		circleLight.disabled = true
+		flash_shape.disabled = false
 		self.offset.x = 15
+		
 		self.texture_scale = 0.8
 	else:
+		circleLight.disabled = false
+		flash_shape.disabled = true
 		self.offset.x = 0
 		self.texture = preload("res://.godot/imported/pointLight.webp-631b7ae0c928feba6faed6027f2cf433.ctex")
